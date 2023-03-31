147b6b796b3c44e6d4610b3e02668f6edb5fc3f8
BJ <95271342+DoctorBJones@users.noreply.github.com>
2023-03-31 21:06:16 +1100

    tidies comments global.R

--- global.R
+++ global.R
@@ -7,8 +7,8 @@ library(tidyverse)
 penguins <- readRDS("data/penguins.rds")
 
 
-# source your functions here
-source("functions/box_green.R") # use the green box if you're using the green theme
+# source functions 
+source("functions/box_green.R") 
 source("functions/gauge_plot_function.R")
 
 #options
